dc922eaaa90a9a287a75731cc8f704c43ad48424
Roman-Ojha <razzroman98@gmail.com>
2022-09-27 18:09:34 +0545

    style: added fonts & svg icons

--- components/Home/Button.tsx
+++ components/Home/Button.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import {StyleSheet, View, TouchableOpacity, Text} from 'react-native';
+// import MaterialIcons from 'react-native-vector-icons/App';
+import {createIconSet} from 'react-native-vector-icons';
+const Icon = createIconSet(
+  {
+    stack: 90,
+  },
+  'app-icons',
+  'app-icons.ttf',
+);
 
 interface ButtonProps {
   title: string;
@@ -10,8 +19,9 @@ const Button: React.FC<ButtonProps> = ({title, icon}): JSX.Element => {
   return (
     <TouchableOpacity>
       <View>
+        {/* <MaterialIcons name="delete" color="black" size={18} /> */}
+        <Icon name="stack" />
         <Text>{title}</Text>
-        <Text>{icon}</Text>
       </View>
     </TouchableOpacity>
   );
