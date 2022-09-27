f3e9836c03943113c6dcccb904d6dfcb0b19887a
Roman-Ojha <razzroman98@gmail.com>
2022-09-27 10:19:02 +0545

    style: added splash screen

--- App.tsx
+++ App.tsx
@@ -1,18 +1,34 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, useColorScheme} from 'react-native';
+import React, {useEffect} from 'react';
+// import {SafeAreaView, StyleSheet, useColorScheme} from 'react-native';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+// import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  // const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  // const backgroundStyle = {
+  //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  // };
 
-  return <SafeAreaView style={backgroundStyle}></SafeAreaView>;
+  // return <SafeAreaView style={backgroundStyle}></SafeAreaView>;
+
+  useEffect(() => {
+    SplashScreen.hide();
+  });
+
+  return (
+    <SafeAreaView>
+      <Text style={styles.text}>Hello</Text>
+    </SafeAreaView>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  text: {
+    color: 'red',
+  },
+});
 
 export default App;
