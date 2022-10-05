c796eb8bc387acdc13d336fa08a756c611f812e2
Roman-Ojha <razzroman98@gmail.com>
2022-10-05 17:57:25 +0545

    feat(SortingVisualizer): implemented Order checkbox

--- src/components/SortingVisualizer/MenuSettings/ArraySizePicker.tsx
+++ src/components/SortingVisualizer/MenuSettings/ArraySizePicker.tsx
@@ -1,80 +1,113 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
-import RNPickerSelect, {Item} from 'react-native-picker-select';
-import {SortingVisualizerState} from '../../../redux/sortingVisualizer/types';
+import RangeSlider from 'rn-range-slider';
+import SelectedRail from '../../RangeSlider/SelectedRail';
+import Rail from '../../RangeSlider/Rail';
+import Thumb from '../../RangeSlider/Thumb';
 import {actionCreators, AppState} from '../../../redux';
 import {useSelector, useDispatch} from 'react-redux';
+import Notch from '../../RangeSlider/Notch';
 import {bindActionCreators} from 'redux';
+import {maximumSortingArraySize} from '../../../redux/sortingVisualizer/types';
 
-interface ItemInterface extends Item {
-  label: SortingVisualizerState['title'];
-  value: SortingVisualizerState['sortingAlgorithm'];
-}
-
-const AlgorithmPicker = (): JSX.Element => {
+const ArraySizePicker = (): JSX.Element => {
   const dispatch = useDispatch();
-  const {sortingAlgorithm} = useSelector(
-    (state: AppState) => state.sortingVisualizer,
-  );
-  const {changeSortingAlgorithm} = bindActionCreators(actionCreators, dispatch);
-  const item: ItemInterface[] = [
-    {
-      label: 'Bubble Sort',
-      value: 'bubble',
-    },
-    {
-      label: 'Insertion Sort',
-      value: 'insertion',
-    },
-    {
-      label: 'Merge Sort',
-      value: 'merge',
-    },
-    {
-      label: 'Quick Sort',
-      value: 'quick',
-    },
-  ];
+  const min: number = 10;
+  const {arraySize} = useSelector((state: AppState) => state.sortingVisualizer);
+  const {changeSortingArraySize} = bindActionCreators(actionCreators, dispatch);
   return (
-    <View style={styles.choseAlgorithmContainer}>
-      <Text style={styles.selectAlgorithmText}>Select an Algorithm:</Text>
-      <RNPickerSelect
-        items={item}
-        onValueChange={value => changeSortingAlgorithm(value)}
-        value={sortingAlgorithm}
-        style={{
-          viewContainer: {
-            borderStyle: 'solid',
-            borderWidth: 1,
-            borderColor: 'gray',
-            borderRadius: 8,
-            height: 40,
-            display: 'flex',
-            justifyContent: 'center',
-          },
-          inputAndroid: {
-            color: 'white',
-          },
-        }}
-        placeholder={{
-          label: 'select an algorithm...',
-          value: null,
-          color: 'gray',
-        }}
-      />
-    </View>
+    <>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>Array Size: </Text>
+          <Text style={styles.speedText}>{arraySize}</Text>
+        </View>
+        <View style={styles.rangeContainer}>
+          <Text style={styles.minText}>{min}</Text>
+          <RangeSlider
+            min={min}
+            max={maximumSortingArraySize}
+            step={1}
+            disableRange
+            floatingLabel={true}
+            low={arraySize}
+            style={styles.speedRangeContainer}
+            renderThumb={useCallback(
+              () => (
+                <Thumb />
+              ),
+              [],
+            )}
+            renderRail={useCallback(
+              () => (
+                <Rail />
+              ),
+              [],
+            )}
+            renderRailSelected={useCallback(
+              () => (
+                <SelectedRail />
+              ),
+              [],
+            )}
+            renderNotch={useCallback(
+              () => (
+                <Notch payload={arraySize} />
+              ),
+              [arraySize],
+            )}
+            onValueChanged={low => {
+              const sortingArraySize: number = low;
+              if (sortingArraySize !== arraySize) {
+                changeSortingArraySize(sortingArraySize);
+              }
+            }}
+          />
+          <Text style={styles.maxText}>{maximumSortingArraySize}</Text>
+        </View>
+      </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
-  choseAlgorithmContainer: {
-    display: 'flex',
+  container: {
+    alignSelf: 'stretch',
   },
-  selectAlgorithmText: {
+  rangeContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 8,
+    padding: 10,
+  },
+  speedRangeContainer: {
+    width: 150,
+  },
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  title: {
     color: 'white',
     margin: 10,
     fontSize: 15,
   },
+  speedText: {
+    color: 'white',
+  },
+  minText: {
+    color: 'white',
+  },
+  maxText: {
+    color: 'white',
+  },
 });
 
-export default AlgorithmPicker;
+export default ArraySizePicker;
--- src/components/SortingVisualizer/MenuSettings/SortingOrderPicker.tsx
+++ src/components/SortingVisualizer/MenuSettings/SortingOrderPicker.tsx
@@ -1,20 +1,46 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
-import RNPickerSelect, {Item} from 'react-native-picker-select';
-import {SortingVisualizerState} from '../../../redux/sortingVisualizer/types';
 import {actionCreators, AppState} from '../../../redux';
 import {useSelector, useDispatch} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import CheckBox from '@react-native-community/checkbox';
+import {stylesConst} from '../../../constants';
 
 const SortingOrderPicker = (): JSX.Element => {
   const dispatch = useDispatch();
-  const {sortingAlgorithm} = useSelector(
-    (state: AppState) => state.sortingVisualizer,
-  );
-  const {changeSortingAlgorithm} = bindActionCreators(actionCreators, dispatch);
+  const {order} = useSelector((state: AppState) => state.sortingVisualizer);
+  const {changeSortingOrder} = bindActionCreators(actionCreators, dispatch);
   return (
     <View style={styles.choseAlgorithmContainer}>
       <Text style={styles.selectAlgorithmText}>Select Order:</Text>
+      <View style={styles.checkBoxContainer}>
+        <View style={styles.ascendingContainer}>
+          <CheckBox
+            value={order === 'ascending' ? true : false}
+            tintColors={{
+              false: 'white',
+              true: stylesConst.colors.primary[1000],
+            }}
+            onChange={() => {
+              changeSortingOrder('ascending');
+            }}
+          />
+          <Text style={styles.ascendingText}>Ascending</Text>
+        </View>
+        <View style={styles.descendingContainer}>
+          <CheckBox
+            value={order === 'descending' ? true : false}
+            tintColors={{
+              false: 'white',
+              true: stylesConst.colors.primary[1000],
+            }}
+            onChange={() => {
+              changeSortingOrder('descending');
+            }}
+          />
+          <Text style={styles.descendingText}>Descending</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -27,6 +53,34 @@ const styles = StyleSheet.create({
     color: 'white',
     margin: 10,
     fontSize: 15,
+  },
+  checkBoxContainer: {
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 8,
+    height: 40,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  ascendingContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  ascendingText: {
+    color: 'white',
+  },
+  descendingContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  descendingText: {
+    color: 'white',
   },
 });
 
