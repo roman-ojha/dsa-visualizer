3c19ad994edb83ba3c46020bdd16ddd32beabf65
Roman-Ojha <razzroman98@gmail.com>
2022-10-07 20:18:29 +0545

    feat: added EditButton Footer in StackVisualizer

---
+++ src/components/StackVisualizer/EditFooter.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {screenConst} from '../../constants';
+import CustomIcons from '../CustomIcons/CustomIcons';
+
+const EditFooter = (): JSX.Element => {
+  return (
+    <>
+      <TouchableOpacity style={styles.container}>
+        <CustomIcons
+          name="arrow-circle"
+          color="black"
+          size={25}
+          style={styles.icon}
+        />
+        <Text style={styles.text}>Edit</Text>
+      </TouchableOpacity>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: screenConst.screenWidth,
+    paddingBottom: 10,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  icon: {
+    transform: [{rotate: '180deg'}],
+  },
+  text: {
+    fontSize: 20,
+    color: 'black',
+  },
+});
+
+export default EditFooter;
--- src/screens/StackVisualizer.tsx
+++ src/screens/StackVisualizer.tsx
@@ -5,6 +5,7 @@ import constants, {screenConst} from '../constants';
 import {mixins} from '../styles';
 import Header from '../components/StackVisualizer/Header';
 import Info from '../components/StackVisualizer/Info';
+import EditFooter from '../components/StackVisualizer/EditFooter';
 
 const StackVisualizer = (): JSX.Element => {
   return (
@@ -15,8 +16,11 @@ const StackVisualizer = (): JSX.Element => {
           constants.styles.colors.secondary2[150],
         ]}
         style={styles.container}>
-        <Header />
-        <Info />
+        <View>
+          <Header />
+          <Info />
+        </View>
+        <EditFooter />
       </LinearGradient>
     </SafeAreaView>
   );
@@ -26,20 +30,8 @@ const styles = StyleSheet.create({
   container: {
     ...mixins.takeFullPage,
     display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {fontSize: 30, color: 'black'},
-  header: {
-    display: 'flex',
-    flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    width: screenConst.screenWidth,
-  },
-  title: {
-    marginRight: 25,
-    fontSize: 25,
   },
 });
 
