041dc70bba3df0080d13ca8552483a48db7cb015
Roman-Ojha <razzroman98@gmail.com>
2022-10-07 23:17:44 +0545

    feat: defined all useful Stack Function

--- src/utils/stack.ts
+++ src/utils/stack.ts
@@ -1,4 +1,5 @@
 import {StackVisualizerState} from '../redux/stackVisualizer/types';
+import generateRandomNumber from './generateRandomNumber';
 
 export const isFull = (state: StackVisualizerState): boolean => {
   if (state.topOfStack === state.stackSize - 1) {
@@ -12,4 +13,53 @@ export const isEmpty = (state: StackVisualizerState): boolean => {
     return true;
   }
   return false;
+};
+
+export const push = (
+  state: StackVisualizerState,
+  data: StackVisualizerState['stack'][0],
+): StackVisualizerState => {
+  if (isFull(state)) {
+    return {
+      ...state,
+      status: 'stack overflow',
+    };
+  }
+  return {
+    ...state,
+    stack: [...state.stack, data],
+    status: `pushed ${data}`,
+  };
+};
+
+export const pushRandomItemIntoStack = (
+  state: StackVisualizerState,
+): StackVisualizerState => {
+  if (isFull(state)) {
+    return {
+      ...state,
+      status: 'stack overflow',
+    };
+  }
+  const data = generateRandomNumber(1, 999);
+  return {
+    ...state,
+    stack: [...state.stack, data],
+    status: `pushed ${data}`,
+  };
+};
+
+export const pop = (state: StackVisualizerState): StackVisualizerState => {
+  if (isEmpty(state)) {
+    return {
+      ...state,
+      status: 'stack underflow',
+    };
+  }
+  const data = state.stack.pop();
+  return {
+    ...state,
+    stack: state.stack,
+    status: `popped ${data}`,
+  };
 };
