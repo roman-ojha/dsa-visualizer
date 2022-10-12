7c07d89e3864aa9c15d0d00fbec49ef9be462efc
Roman-Ojha <razzroman98@gmail.com>
2022-10-12 20:41:09 +0545

    feat(redux): defined type for QueueVisualizer

---
+++ src/redux/queueVisualizer/types.ts
@@ -0,0 +1,31 @@
+export interface QueueVisualizerState {
+  size: number;
+  front: number;
+  rare: number;
+  queue: number;
+  status: string | null;
+}
+
+export enum QueueVisualizerActionTypes {
+  ENQUEUE_Into_QUEUE = 'ENQUEUE_Into_QUEUE',
+  DEQUEUE_FROM_QUEUE = 'DEQUEUE_FROM_QUEUE',
+  ENQUEUE_RANDOM_VALUE_INTO_QUEUE = 'ENQUEUE_RANDOM_VALUE_INTO_QUEUE',
+}
+
+export interface EnqueueIntoQueue {
+  type: QueueVisualizerActionTypes.ENQUEUE_Into_QUEUE;
+  data: number;
+}
+
+export interface DequeueFromQueue {
+  type: QueueVisualizerActionTypes.DEQUEUE_FROM_QUEUE;
+}
+
+export interface EnqueueRandomValueIntoQueue {
+  type: QueueVisualizerActionTypes.ENQUEUE_RANDOM_VALUE_INTO_QUEUE;
+}
+
+export type StackVisualizerAction =
+  | EnqueueIntoQueue
+  | DequeueFromQueue
+  | EnqueueRandomValueIntoQueue;
